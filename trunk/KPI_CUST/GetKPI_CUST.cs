7b698d4537e7c3325d8a44fcd614762bd2553192
taoataoabao <taoataoabao@3ad23166-5d0e-ed3f-bb8c-5fc7ae23534f>
2013-04-19 08:19:41 +0000

    create

---
+++ trunk/KPI_CUST/GetKPI_CUST.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KPI_CUST
+{
+    /// <summary>
+    /// 此为任务1
+    /// 功能：以后采用windows任务，配置执行exe的执行时间即可。
+    /// 执行时间：每日 00:10，获取昨天的数据
+    /// 启动之后，获取kpi数据库中当天未购买
+    /// 或者拍单但是未付款的客户信息
+    /// 之后插入配置好的数据库中，表：t_kpi_cust
+    /// </summary>
+   
+    public partial class GetKPI_CUST : Form
+    {
+        public GetKPI_CUST()
+        {
+            InitializeComponent();
+        }
+
+        private void GetKPI_CUST_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
